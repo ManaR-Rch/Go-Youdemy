5a25714cead5df9c0363364a87eaf4eaf5a40f86
ManaR-Rch <manarmarchou6@gmail.com>
2025-03-01 07:44:34 +0000

    setting classes

--- Views/Admin/dashboardAdmin.php
+++ Views/Admin/dashboardAdmin.php
@@ -40,7 +40,7 @@ $topTeachers = $db->getTopTeachers();
                     </li>
                     <li>
                         <a href="dashUser.php" class="flex items-center px-4 py-3 text-gray-700 hover:bg-gray-50 rounded-lg">
-                            👥 Utilsateurs
+                            👥 Utilisateurs
                         </a>
                     </li>
                     <li>
